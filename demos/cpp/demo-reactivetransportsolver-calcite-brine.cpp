c39250c9570fd504d21fbe6acc9b666a81c686a8
Svetlana Kyas <mtsveta@gmail.com>
2019-04-25 16:27:15 +0200

    tutorial for the reactivetransportsolver is added

--- demos/cpp/demo-reactivetransportsolver-calcite-brine.cpp
+++ demos/cpp/demo-reactivetransportsolver-calcite-brine.cpp
@@ -15,36 +15,14 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with this library. If not, see <http://www.gnu.org/licenses/>.
 
-// C++ includes
-#include <algorithm>    // for using transform
-
-#if defined _WIN32      // for creating a new folder
-#include <windows.h>
-#ifdef __MINGW32__
-#include <sys/stat.h>
-#endif
-#else
-#include <sys/stat.h>
-#endif
-
-// Reaktoro includes
 #include <Reaktoro/Reaktoro.hpp>
+#include <Reaktoro/Transport/TransportSolver.hpp>
+
+//#include <boost/filesystem.hpp>
+//#include <sys/stat.h>
+#include <cstdlib>
 
 using namespace Reaktoro;
-
-/// Make directory for Windows and Linux
-bool mkdir(const std::string & folder)
-{
-#if defined _WIN32
-    // Replace slash by backslash
-    std::transform(begin(folder), end(folder), begin(folder),
-                   [](char ch) { return ch == '/' ? '\\' : ch; });
-    return 0 != CreateDirectory(folder.c_str(), NULL);
-#else
-    // Create the directory with Read + Write + Execute rights for user, group, and others
-    return ::mkdir(folder.c_str(), S_IRWXU | S_IRWXG | S_IRWXO);
-#endif
-}
 
 int main()
 {
@@ -54,16 +32,14 @@ int main()
 
     // Step 2: Define parameters for the reactive transport simulation
     double xl(0.0), xr(100.0);          // the x-coordinates of the left and right boundaries
-    int nsteps(100);                    // the number of steps in the reactive
-    // transport simulation
+    int nsteps(100);                    // the number of steps in the reactive transport simulation
     int ncells(100);                    // the number of cells in the spacial discretization
     double D(1.0e-9);                   // the diffusion coefficient (in units of m2/s)
     double v(1.0 / day);                // the fluid pore velocity (in units of m/s)
     double dt(0.5 * day);               // the time step (in units of s)
     double T(60.0);                     // the temperature (in units of degC)
     double P(100);                      // the pressure (in units of bar)
-    double is_smrtsolv(false);          // the parameter that defines whether
-    // classic or smart EquilibriumSolver must be used
+    double smart_solver(true);          // the parameter that defines whether classic or smart EquilibriumSolver must be used
 
     // Step 3: Construct the chemical system with its phases and species (using ChemicalEditor)
     ChemicalEditor editor;
@@ -114,7 +90,7 @@ int main()
     ChemicalField field(mesh.numCells(), state_ic);
 
     // Step 12: Define the reactive transport modeling
-    ReactiveTransportSolver rtsolver(system, is_smrtsolv);
+    ReactiveTransportSolver rtsolver(system);
     rtsolver.setMesh(mesh);
     rtsolver.setVelocity(v);
     rtsolver.setDiffusionCoeff(D);
@@ -132,29 +108,11 @@ int main()
     output.add("speciesMolality(CO2(aq))");
     output.add("phaseVolume(Calcite)");
     output.add("phaseVolume(Dolomite)");
-
-    // Step **: Create the results folder
-    struct stat status = {0};               // structure to get the file status
-    // Name of the folder with results
-    std::string test_tag = "-ncells-" + std::to_string(ncells) +
-                           "-nsteps-" + std::to_string(nsteps) +
-                           "-ismart-" + (is_smrtsolv == true ? "1" : "0");
-    std::string folder = "../results" + test_tag;
-
-    if (stat(folder.c_str(), &status) == -1) mkdir(folder.c_str()); // create the
-    output.filename(folder + "/" + "test.txt");
-
-    // Step **: Create two profilers
-    Profiler rt_profiler(rtsolver.profile(Profiling::RT));
-    Profiler eq_profiler(rtsolver.profile(Profiling::EQ));
-    Profiler total_profiler(Profiling::Total);
+    output.filename("TEST.txt");
 
     // Step 15: Set initial time and counter of steps in time
     double t(0.0);
     int step(0);
-
-    // Step **: Start profiling total simulation
-    total_profiler.startProfiling();
 
     // Reactive transport simulations in the cycle
     while (step <= nsteps){
@@ -162,20 +120,11 @@ int main()
         std::cout << "Progress: " << step << " / " << nsteps << " "  << t/minute << " min" << std::endl;
 
         // Perform one reactive transport time step
-        rtsolver.step(field, is_smrtsolv);
+        rtsolver.step(field);
 
         // Increment time step and number of time steps
         t += dt;
         step += 1;
     }
-    // Step **: End profiling total simulation
-    total_profiler.endProfiling();
 
-    // Step **: Output the total time of the simulations
-    std::cout << "Elapsed time: ";
-    total_profiler.consoleOutput();
-    std::cout << std::endl;
-
-    // Output profiling results
-    rtsolver.outputProfiling(folder + "/profiling");
 }
