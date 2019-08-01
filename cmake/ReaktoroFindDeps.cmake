7c90ed3ecd6d3bb044cc38697bd1e7b26c033de0
Allan Leal <allanmulin@gmail.com>
2019-08-01 14:24:34 +0200

    Using json lib from conda-forge

--- cmake/ReaktoroFindDeps.cmake
+++ cmake/ReaktoroFindDeps.cmake
@@ -1,21 +1,14 @@
-# Find Boost library
 find_package(Boost REQUIRED)
+find_package(nlohmann_json 3.6.1 REQUIRED)
 
-# Find ThermoFun library and define a pre-processor macro REAKTORO_USING_THERMOFUN if so
-find_package(ThermoFun REQUIRED)
-
-if(ThermoFun_FOUND)
-    add_compile_definitions(REAKTORO_USING_THERMOFUN)
-endif()
-
-# Find pybind11 library (if needed)
 if(REAKTORO_BUILD_PYTHON)
-    set(PYBIND11_CPP_STANDARD -std=c++17)  # Ensure pybind11 really uses c++17
-    find_package(pybind11 REQUIRED)
+
+    find_package(pybind11 2.3.0 REQUIRED)
     if(NOT pybind11_FOUND)
         message(WARNING "Could not find pybind11 - the Python module `reaktoro` will not be built.")
         set(REAKTORO_BUILD_PYTHON OFF)
     else()
         message(STATUS "Found pybind11 v${pybind11_VERSION}: ${pybind11_INCLUDE_DIRS}")
     endif()
+
 endif()
